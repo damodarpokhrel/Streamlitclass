0cdba7d07d41b2f5bc09cc481c57ce4f0d3daad2
Damodar Pokhrel <agrotecn@gmail.com>
2025-05-02 13:25:04 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import steamlit as st
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris, load_wine, load_breast_cancer
+
+st.tile("Broadway infosis app")
+st.write("Explore diffferent classifiers and dataset which one is best for you")
+dataset = st.selectbox("Select dataset", ["Iris", "Wine", "Breast Cancer"])
+st.write(f"## {dataset_name} dataset")
+classifier_name = st.slidebar.selectbox('Select classifier', ['KNN', 'SVM', 'Random Forest'])
+
+def get_dataset(dataset_name):
+    if dataset_name == 'Iris':
+        iris = datasets.load_iris()
+        X = iris.data
+        y = iris.target
+    elif dataset_name == 'Wine':
+        wine = datasets.load_wine()
+        X = wine.data
+        y = wine.target
+    else:
+        breast_cancer = datasets.load_breast_cancer()
+        X = breast_cancer.data
+        y = breast_cancer.target
+    return X, y
+x, y = get_dataset(dataset_name)
+st.write("Shape of dataset", X.shape)
+st.write("Number of classes", len(np.unique(y)))
+
+def add_parameter_ui(classifier_name):
+    params = dict()
+    if classifier_name == 'KNN':
+        K = st.slider("K", 1, 15)
+        params['K'] = K
+    elif classifier_name == 'SVM':
+        C = st.slider("C", 0.01, 10.0)
+        params['C'] = C
+    else:
+        max_depth = st.slider("max_depth", 2, 15)
+        n_estimators = st.slider("n_estimators", 1, 100)
+        params['max_depth'] = max_depth
+        params['n_estimators'] = n_estimators
+    return params
+passs = add_parameter_ui(classifier_name, params)
+
+def get_classifier(classifier_name, params):
+    if classifier_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    elif classifier_name == 'SVM':
+        clf = SVC(C=params['C'])
+    else:
+        clf = RandomForestClassifier(n_estimators=params['n_estimators'], max_depth=params['max_depth'], random_state=1234)
+    return clf
+clf = get_classifier(classifier_name, add_parameter_ui(classifier_name))
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+acc = accuracy_score(y_test, y_pred)
+st.write("Classifier = ", classifier_name)
+st.write("Accuracy = ", acc)
+st.write("Parameters = ", params)
+
+pca = PCA(2)
+X = pca.fit_transform(X)
+
+x1 = X_projeceted[:, 0]
+x2 = X_projecoted[:, 1]
+
+fig = plt.figure(figsize=(8, 6))
+plt.scatter(x1, x2, alpha=0.5)
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.title('PCA of Dataset')
+st.pyplot(fig)
+plt.show()
+
+
+
+
